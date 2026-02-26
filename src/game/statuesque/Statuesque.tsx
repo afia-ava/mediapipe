a379ece52cfb7c6e6b375c4522a4cc2b2a4e3631
Afia Khanom Ava <afiakhanomava@gmail.com>
2026-02-26 17:11:39 -0600

    statuesque intro fixed

--- src/game/statuesque/Statuesque.tsx
+++ src/game/statuesque/Statuesque.tsx
@@ -3,19 +3,24 @@ import PoseGame from "./PoseGame";
 import { Leaderboard } from "./Leaderboard"; 
 import "./GamePage.css";
 
-import pose1 from "../../assets/pose1.jpg";
-import pose2 from "../../assets/pose2.jpg";
-import pose3 from "../../assets/pose3.jpg";
-import pose4 from "../../assets/pose4.jpg";
-import pose5 from "../../assets/pose5.jpg";
-import pose6 from "../../assets/pose6.jpg";
-import pose7 from "../../assets/pose7.jpg";
+import intro1 from "./assets/intro1.png";
+import intro2 from "./assets/intro2.png";
+import intro3 from "./assets/intro3.png";
+
+import pose1 from "./assets/pose1.jpg";
+import pose2 from "./assets/pose2.jpg";
+import pose3 from "./assets/pose3.jpg";
+import pose4 from "./assets/pose4.jpg";
+import pose5 from "./assets/pose5.jpg";
+import pose6 from "./assets/pose6.jpg";
+import pose7 from "./assets/pose7.jpg";
 
 export default function GamePage() {
   const [showIntro, setShowIntro] = useState(true);
-  const SLIDES = [pose1, pose2, pose3, pose4, pose5, pose6, pose7];
+  const INTRO_SLIDES = [intro1, intro2, intro3];
   const [slideIndex, setSlideIndex] = useState(0);
 
+  const POSE_IMAGES = [pose1, pose2, pose3, pose4, pose5, pose6, pose7];
   return (
     <div className="game-page">
       {showIntro && (
@@ -29,12 +34,12 @@ export default function GamePage() {
           </button>
 
           <div className="intro-content">
-            <img src={SLIDES[slideIndex]} alt={`Intro ${slideIndex+1}`} className="intro-image" />
+            <img src={INTRO_SLIDES[slideIndex]} alt={`Intro ${slideIndex+1}`} className="intro-image" />
             <div className="intro-actions">
-              {slideIndex < SLIDES.length - 1 ? (
+              {slideIndex < INTRO_SLIDES.length - 1 ? (
                 <button
                   className="intro-next"
-                  onClick={() => setSlideIndex((s) => Math.min(s + 1, SLIDES.length - 1))}
+                  onClick={() => setSlideIndex((s) => Math.min(s + 1, INTRO_SLIDES.length - 1))}
                 >
                   Next
                 </button>
@@ -46,10 +51,10 @@ export default function GamePage() {
                   Start
                 </button>
               )}
-            </div>
-          </div>
-        </div>
-      )}
+            </div> 
+          </div> 
+        </div> 
+      )} 
 
       <main className="game-main">
         <section className="left">
